4dc2c777e95c7506c54cecef33e7761b38338b11
wwwal2 <smalex9@gmail.com>
2019-12-11 13:10:17 +0200

    random generator bug fixed

--- src/game/Structure.js
+++ src/game/Structure.js
@@ -3,30 +3,36 @@ import Utility from './Utility';
 
 class Structure {
   constructor() {
-    this.table = [];
-    this.gameElements = [];
-    this.image = {};
+    this.tableOfmatches = [];
+    this.location = {};
+    this.randomValues = [];
   }
 
   Build(settings) {
+    this.tableOfmatches = [];
     const btnNum = settings.height * settings.width;
     const odd = btnNum % 2;
-    const randomValues = Utility.generateRandomValues(Math.floor(btnNum / 2));
+    this.randomValues = Utility.generateRandomValues(Math.floor(btnNum / 2));
 
-    const structure = Utility.selectElements('field-container')[0];
-
+    [this.location] = Utility.selectElements('field-container');
     for (let i = 1; i <= settings.height; i += 1) {
       const row = Utility.createElement('div', 'row');
-      this.table.push([]);
+      this.tableOfmatches.push([]);
       for (let j = 1; j <= settings.width; j += 1) {
         if (odd && i === Number(settings.height) && j === Number(settings.width)) {
           break;
         }
         const button = new Button.Create('cells', i, j);
         row.appendChild(button);
-        this.table[i - 1].push(randomValues.pop());
+        this.tableOfmatches[i - 1].push(this.randomValues.pop());
       }
-      structure.appendChild(row);
+      this.location.appendChild(row);
+    }
+  }
+
+  Remove() {
+    if (this.location.childNodes) {
+      this.location.innerHTML = '';
     }
   }
 }
--- src/initGame.js
+++ src/initGame.js
@@ -29,19 +29,20 @@ const initGame = (settings) => {
   };
 
   const start = () => {
+    timer.clear();
+    structure.Remove();
+
     const playerSettings = {
       height: controllers.height.value.innerText,
       width: controllers.width.value.innerText,
       preview: controllers.preview.value.innerText,
       gameOver: controllers.gameOver.value.innerText,
     };
-    timer.clear();
     timer.start(controllers.gameOver.value.innerText);
     structure.Build(playerSettings);
+    gameStatus.tableOfmatches = structure.tableOfmatches;
     Utility.addEvent(clickImage, 'cells');
-    gameStatus.tableOfmatches = structure.table;
   };
-
   Utility.addEvent(start, 'start');
 };
 
